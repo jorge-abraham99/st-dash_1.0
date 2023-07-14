804f84268f026f3bdc56683c08fc49a2ec9148c3
Jorge <abrahamjorge22@gmail.com>
2023-07-14 18:32:31 -0400

    V1.01

--- Indicators.py
+++ Indicators.py
@@ -6,6 +6,7 @@ from tasa import Tasa_BCV, reservas_excedentes,liquidity
 
 #rv = reservas_excedentes('https://www.bcv.org.ve/sites/default/files/EstadisticasGeneral/1_1_4.xls')
 liq = liquidity('https://www.bcv.org.ve/sites/default/files/indicadores_sector_monetario/liquidez_monetaria_semanal1.xls').df
+df1 = prices().df
 st.set_page_config(
 page_title = 'Indicadores en Tiempo Real', page_icon = 'Active',
 layout = 'wide')
@@ -18,7 +19,7 @@ col2.metric("Tasa BCV EUR", Tasa_BCV('https://www.bcv.org.ve').eur)
 
 st.markdown('### Precios Binance')
 
-st.dataframe(prices.get_df(Tipo_de_orden))
+st.dataframe(df1.loc[df1['Tipo'] == Tipo_de_orden])
 
 
 
